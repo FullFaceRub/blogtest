f6249f4dc96252bd457ae194c092ba805ba9bcce
FullFaceRub <danielhbury@gmail.com>
2018-04-16 18:45:32 -0700

    all done except posts are not posting to the api, asking david if I need to implement a backend and db

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,9 +1,25 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
+import Modal from 'react-modal';
+
+const modalStyle = {
+    overlay: {
+        background: "rgba(0,0,0,.25)"
+    },
+    content: {
+        height: "400px",
+        width: "400px",
+        background: "white",
+        margin: "auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around"
+    }
+}
 
 class NewPost extends Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
@@ -13,21 +29,23 @@ class NewPost extends Component {
         this.handleTitle = this.handleTitle.bind(this);
         this.handleBody = this.handleBody.bind(this);
         this.submitPost = this.submitPost.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
-    handleBody(e){
+    handleBody(e) {
         this.setState({
             title: e
         })
     }
 
-    handleTitle(e){
+    handleTitle(e) {
         this.setState({
             body: e
         })
     }
 
-    submitPost(){
+    submitPost() {
         let body = {
             userId: 1,
             title: this.state.title,
@@ -35,21 +53,38 @@ class NewPost extends Component {
         }
 
         axios.post('https://jsonplaceholder.typicode.com/posts', body).then(resp => {
-            console.log(resp);
-            window.location.assign('http://localhost:3000/')
+            this.openModal();
+        })
+    }
+    
+    openModal() {
+        this.setState({
+            modalIsOpen: true
+        })
+    }
+    
+    closeModal() {
+        window.location.assign('http://localhost:3000')
+        this.setState({
+            modalIsOpen: false
         })
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="postbody">
-            <div className="inputfield">
-                <input type="text" onChange={e=>this.handleBody(e.target.value)}/>
-            </div>
-            <div className="inputfield">
-                <input type="text" onChange={e=>this.handleTitle(e.target.value)}/>
-            </div>
-            <button onClick={this.submitPost}>Submit</button>
+                <h1>Create a New Post Below</h1>
+                <div className="inputfield">
+                    Post Title: <input type="text" onChange={e => this.handleBody(e.target.value)} />
+                </div>
+                <div className="inputfield">
+                    Post Body: <input className="bodyinput" type="text" onChange={e => this.handleTitle(e.target.value)} />
+                </div>
+                <button onClick={this.submitPost}>Submit</button>
+                <Modal onRequestClose={this.closeModal} isOpen={this.state.modalIsOpen} style={modalStyle}>
+                    <button onClick={this.closeModal}>Close</button>
+                    <h1>Thank you for submitting a new post!</h1>
+                </Modal>
             </div>
         )
     }
@@ -57,7 +92,7 @@ class NewPost extends Component {
 
 function mapStateToProps(state) {
     return {
-        
+
     }
 }
 
